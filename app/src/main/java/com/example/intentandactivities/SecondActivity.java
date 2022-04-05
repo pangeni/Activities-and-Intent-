db27172ec397c2a8b361ea96935a5e116ac24877
pangeni <pangenisanjay@gmail.com>
2022-04-05 18:26:40 +0545

    merge sucessfully

--- app/src/main/java/com/example/intentandactivities/SecondActivity.java
+++ app/src/main/java/com/example/intentandactivities/SecondActivity.java
@@ -1,14 +1,5 @@
 package com.example.intentandactivities;
 
-<<<<<<< HEAD
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-
-public class SecondActivity extends AppCompatActivity {
-
-=======
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -18,19 +9,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class SecondActivity extends AppCompatActivity {
 
     private TextView text_count;
->>>>>>> Activity_Homework
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-<<<<<<< HEAD
-
-        Intent intent=getIntent();
-        String msg = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-    }
-
-}
-=======
         text_count = findViewById(R.id.view_count);
 
 
@@ -41,4 +23,3 @@ public class SecondActivity extends AppCompatActivity {
 
 
 }
->>>>>>> Activity_Homework
